32f0948aec54c7c88d1f2c1b98ce76a0fe197d1e
Brayan Godoy <brayanestivengodoy15@gmail.com>
2023-11-09 01:24:44 -0500

    Boton

--- Parte_1/form_busqueda.php
+++ Parte_1/form_busqueda.php
@@ -4,14 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Búsqueda de Productos</title>
-    <link rel="stylesheet" type="text/css" href="estilos.css?ver=1.2">
+    <link rel="stylesheet" type="text/css" href="estilos.css?ver=1.1">
     <link rel="stylesheet" href="">
 </head>
 <body>
     <div class="main-content">
-        <form class="form-search" method="get" action="result_busqueda.php" >
+        <form class="form-search" method="get" action="result_busqueda.php">
             <input type="search" name="search" placeholder="Buscar Producto..." autofocus required>
             <button type="submit">Buscar</button>
+            <a href="consulta.php" class="ir-button">Volver a consulta</a>
         </form>
     </div>    
 </body>

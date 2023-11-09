ab6dd35c043b10dce4bfa5b891b4960c12e134d4
Brayan Godoy <brayanestivengodoy15@gmail.com>
2023-11-09 01:03:27 -0500

    Actualizando busqueda

--- Parte_1/consulta2.php
+++ Parte_1/consulta2.php
@@ -6,16 +6,8 @@
 <body>
 
     <?php
-    $mipagina=$_SERVER["PHP_SELF"];
-        
-
     require_once("conexionbd.php");
-
-    $selector =isset($_GET["selector"]);
-    if ($selector == "1") {
-        $selector="producto";
-        $busca="Tubos";
-        $consulta = "SELECT * FROM productos WHERE '$selector' ='%$busca%'";
+        $consulta = "SELECT * FROM productos WHERE producto ='CERAMICA'";
         $resultado = mysqli_query($conexion, $consulta);
         echo '<table>';
         while ($fila = mysqli_fetch_row($resultado)) {
@@ -28,26 +20,6 @@
         echo '</table>';
         
         mysqli_close($conexion);
-    }
-
-echo $selector;
-    if (isset($_GET["selector"])) {
-        
-    }
-
     ?>
-    <div class='main-content'>
-    <form class='form-search' method='get' action='<?php $mipagina ?>' >
-            <select name="selector" id="lang" style="font-size: 30px; " required>
-                <option></option>
-                <option value="producto">Socio</option>
-                <option value="8">Asociado</option>
-            </select>
-            <button type='submit'>Buscar</button>
-
-        </form>
-    </div>
-
-
 </body>
 </html>

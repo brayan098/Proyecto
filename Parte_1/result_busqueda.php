9be08fb753bb70f1aec7155e19d9a626517ba1cb
Brayan Godoy <brayanestivengodoy15@gmail.com>
2023-11-09 00:59:38 -0500

    Añadiendo campos para la tabla

--- Parte_1/result_busqueda.php
+++ Parte_1/result_busqueda.php
@@ -5,25 +5,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Productos Encontrados</title>
     <link rel="stylesheet" href="estilos.css">
+
 </head>
 <body>
+    <table>
+    <tr>
+            <th>ID</th>
+            <th>Seccion</th>
+            <th>Producto</th>
+            <th>Origen</th>
+            <th>Importado</th>
+            <th>Precio</th>
     <?php
     require_once("conexionbd.php");
     $busqueda = $_GET["search"];
     $consulta = "SELECT * FROM productos WHERE producto LIKE '%$busqueda%'";
     $resultado2 = mysqli_query($conexion, $consulta);
 
-    echo '<table>';
+    
     while ($fila = mysqli_fetch_row($resultado2)) {
         echo "<tr>";
         foreach ($fila as $valor) {
-            echo "<td>$valor</td>";
+            echo "<td>" .$valor. "</td>";
         }
         echo "</tr>";
     }
-    echo '</table>';
+   
 
     mysqli_close($conexion);
     ?>
+    </tr>
+    </table>
 </body>
 </html>

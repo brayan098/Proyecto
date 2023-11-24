e1afa6c24f0ccce6224cc472b484974a1d0aaf80
Brayan Godoy <brayanestivengodoy15@gmail.com>
2023-11-23 21:36:34 -0500

    creando formulario de inicio de sesion

--- Parte_3/sesiones/pag_inicio.php
+++ Parte_3/sesiones/pag_inicio.php
@@ -6,6 +6,33 @@
     <title>FORMULARIO</title>
 </head>
 <body>
-    
+    <style>
+        form input[type="submit"] { 
+            margin-left: 44px; 
+            text-decoration: none;
+            width: 100px;
+            height: 30px;
+            border: 1px solid #d7d7d7;
+            background: #8F8E8C;
+        }
+            form input[type="password"], form input[type="email"] {
+            display: table-cell;
+            width: 300px;
+            height: 20px;
+            border: 1px solid #d7d7d7;
+            border-radius: 3px;
+        }
+    </style>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        <div>
+            <label>Usuario: </label>
+            <input type="email" name="correo" autofocus required><br>
+
+            <label>Contraseña: </label>
+            <input type="password" name="password" required><br><br>
+
+            <input type="submit" name="enviar" value="ENVIAR">
+        </div>
+    </form>
 </body>
 </html>

9199b3f070b631c6e0afd14a57d3b1978f3bddc4
Brayan Godoy <brayanestivengodoy15@gmail.com>
2023-12-10 18:58:03 -0500

    Añadiendo archivos y haciendo registro

--- Parte_3/sesiones/registro.php
+++ Parte_3/sesiones/registro.php
@@ -1,16 +1,16 @@
 <html>
-            <head>
-            <title>REGISTRO USUARIO</title>
-            <meta charset="utf8">
+    <head>
+        <title>REGISTRO USUARIO</title>
+        <meta charset="utf8">
           
-            </head>
-            <body>
+    </head>
+        <body>
     
-            <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post" autocomplete="off">
-                <h3>INGRESE CON SUS DATOS</h3>
+        <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post" autocomplete="off">
+            <h3>INGRESE CON SUS DATOS</h3>
     
-            <p>Correo:<input type="text" name="correo" id="correo"/></p>  
-            <p>Contraseña:<input type="password" name="Contra" id="Contra"/></p>
+        <p>Correo: <input type="text" name="correo" id="correo"/></p>  
+        <p>Contraseña: <input type="password" name="Contra" id="Contra"/></p>
     
             <script>
                 $(document).ready(function ($) {
@@ -25,28 +25,28 @@
             </script>
            
            
-            <?php
+    <?php
 
-            $error_encontrado="";
-            $vali="";
-            require_once("validacion_contra.php");
-            if(isset($_POST["enviar"])){
-                if(validar_clave($_POST['Contra'],$error_encontrado)){
-                    echo "Contraseña segura"."<br>";
-                    $base= new PDO("mysql:host=localhost; dbname=datos_usuario", "root","");
-                    $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $error_encontrado="";
+        $vali="";
+        require_once("validacion_contra.php");
+        if(isset($_POST["enviar"])){
+            if(validar_clave($_POST['Contra'],$error_encontrado)){
+                echo "Contraseña segura"."<br>";
+                $base= new PDO("mysql:host=localhost; dbname=datos_usuario", "root","");
+                $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 
-                    $sql="INSERT into datos_usuarioo (usuario,password1) values (:login,:password)";
-                    $resultado=$base->prepare ($sql);
+                $sql="INSERT into datos_usuarioo (usuario,password1) values (:login,:password)";
+                $resultado=$base->prepare ($sql);
 
-                    $corre=htmlentities(addslashes($_POST['correo']));
-                    $contra=htmlentities(addslashes($_POST['Contra'])); 
+                $corre=htmlentities(addslashes($_POST['correo']));
+                $contra=htmlentities(addslashes($_POST['Contra'])); 
 
-                    $resultado->bindValue(":login", $corre);
+                $resultado->bindValue(":login", $corre);
                 // Spassword-password_hash($contraseña->getClave(), PASSWORD_DEFAULT);
                     
-                    $resultado->bindValue(":password",$contra);
-                    $resultado->execute();
+                $resultado->bindValue(":password",$contra);
+                $resultado->execute();
 
     
                     echo "Usuario creado";
@@ -56,10 +56,10 @@
                 }
             }
                 echo "<br>";
-            ?>
-             <input type="submit" value="Enviar" name="enviar"/>
-            </form>
+        ?>
+        <input type="submit" value="Enviar" name="enviar"/>
+    </form>
             
-            </body>
-            </html>
+    </body>
+</html>
 
--- Parte_3/sesiones/validacion_contra.php
+++ Parte_3/sesiones/validacion_contra.php
@@ -3,11 +3,11 @@
 
 function validar_clave($clave,&$error_clave){
         if(strlen($clave)< 6){
-            $error_clave="La contraseña debe tener al menos 6 caracteres";
+            $error_clave="La contraseña debe tener al menos 6 carácteres";
             return false;
         }
         if (strlen($clave)> 16) {
-            $error_clave="La contraseña no debe tener más de 16 caracteres";
+            $error_clave="La contraseña no debe tener más de 16 carácteres";
             return false;
         }
         if (!preg_match('/[a-z]/',$clave)) {
@@ -15,11 +15,11 @@ function validar_clave($clave,&$error_clave){
             return false;
         }
         if (!preg_match('/[A-Z]/',$clave)) {
-            $error_clave="La contraseña debe tener al menos una letra minuscula";
+            $error_clave="La contraseña debe tener al menos una letra minúscula";
             return false;
         }
         if (!preg_match('/[0-9]/',$clave)) {
-            $error_clave="La contraseña debe tener al menos un carater numerico";
+            $error_clave="La contraseña debe tener al menos un carater numérico";
             return false;
         }
 

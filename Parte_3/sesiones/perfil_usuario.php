0a03e220130d29bd21137ca83396c7b8df404df1
Brayan Godoy <brayanestivengodoy15@gmail.com>
2023-12-10 17:31:28 -0500

    Modificando Perfil de usuario

--- Parte_3/sesiones/perfil_usuario.php
+++ Parte_3/sesiones/perfil_usuario.php
@@ -3,21 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/menu.css?ver=1">
-    <title>perfil_usuario</title>
+    <link rel="stylesheet" href="../css/estilos.css">
+    <title>Perfil </title>
 </head>
 <nav>
         <ul class="menu">
             <li><a href="#">Procesos</a>
                 <ul class="submenu">
-                        <li><a href="/DesarrolloWeb/parte1/paginas/busquedaPro.php">Buscar producto</a></li>
-                        <li><a href="/DesarrolloWeb/parte2/productos/formulario.php">Agregar producto</a></li>
+                        <li><a href="/Proyecto/Parte_1/form_busqueda.php">Buscar producto</a></li>
+                        <li><a href="/Proyecto/Parte_2/Productos/insertar_productos.php">Agregar producto</a></li>
                 </ul>
             </li>
-            <li><a href="pag_inicio.php">Inicio</a></li>
+            <li><a href="pagina_inicio.php">Inicio</a></li>
             <li><a href="#">Nosotros</a></li>
             <li><a href="#">Contacto</a></li>
-            <li><a href="eliminar_cookie.php">Eliminar cookie</a></li>
+            <li><a href="eliminarcookie.php">Eliminar cookie</a></li>
 
         </ul>
     </nav>

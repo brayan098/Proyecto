ba3abfb1823c5bae7d90371a378361fc172e9ab2
Brayan Godoy <brayanestivengodoy15@gmail.com>
2023-12-10 19:14:11 -0500

    añadiendo estilos

--- Parte_3/sesiones/form_acceso.php
+++ Parte_3/sesiones/form_acceso.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/estilos.css">
+    <link rel="stylesheet" href="../css/estilos.css?ver=1.0">
     <title>Formulario de acceso</title>
     
 </head>
@@ -33,7 +33,10 @@
         </div>
         </form>
         <br>
-        <label>No tengo cuenta<a href="registro.php">Crear cuenta</a></label>
+        <div class="botonreg">
+        <label>No tengo cuenta</label>
+        <a href="registro.php">Crear cuenta</a>
+    </div>
     </div>
 </body>
 </html>
